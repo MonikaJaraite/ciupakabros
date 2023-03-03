66b66c3811fb6186604ae88f12721524967dbccf
MonikaJaraite <monjara@gmail.com>
2023-03-03 15:46:01 +0200

    Unfinished ex. 11

---
+++ 009/hw11.php
@@ -0,0 +1,15 @@
+<?php
+
+// Papildomas.
+// Naudokite funkcija rand(). Sugeneruokite 6 kintamuosius su atsitiktinem reikšmėm nuo 1000 iki 9999. Atspausdinkite reikšmes viename strige, išrūšiuotas nuo didžiausios iki mažiausios, atskirtas tarpais. Naudoti ciklų ir masyvų NEGALIMA.
+
+$a = rand(1000, 9999);
+$b = rand(1000, 9999);
+$c = rand(1000, 9999);
+$d = rand(1000, 9999);
+$e = rand(1000, 9999);
+$f = rand(1000, 9999);
+
+echo '6 kintamieji: ' . "$a, $b, $c, $d, $e, $f" . '<br>';
+
+echo max($a, $b, $c, $d, $e, $f);

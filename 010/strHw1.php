e665534b25733ecef97906f00e9f9dcf85ba4305
MonikaJaraite <monjara@gmail.com>
2023-03-03 21:18:37 +0200

    homework str ex. 2

--- 010/strHw1.php
+++ 010/strHw1.php
@@ -1,5 +1,7 @@
 <?php
 
+// Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus (Jonas Jonaitis). Atspausdinti trumpesnį stringą.
+
 $name = 'Goldie';
 $surname = 'Hawn';
 
---
+++ 010/strHw2.php
@@ -0,0 +1,8 @@
+<?php
+
+// Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. Vardą atspausdinti tik didžiosiomis raidėmis, o pavardę tik mažosiomis.
+
+$name = 'Goldie';
+$surname = 'Hawn';
+
+echo strtoupper($name) . ' ' . strtolower($surname);

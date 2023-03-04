27e70077681cf328c6a491ebd0bde2af1dd2a02f
MonikaJaraite <monjara@gmail.com>
2023-03-04 10:29:36 +0200

    homework str ex. 7

--- 010/strHw7.php
+++ 010/strHw7.php
@@ -3,7 +3,7 @@
 // Sukurti kintamąjį su stringu: “An American in Paris”. Jame ištrinti visas balses. Rezultatą atspausdinti. Kodą pakartoti su stringais: “Breakfast at Tiffany's”, “2001: A Space Odyssey” ir “It's a Wonderful Life”.
 
 $kint = 'An American in Paris';
-$vowels = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
+$vowels = array('a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y');
 $onlyconsonants = str_replace($vowels, "", $kint);
 
 echo $onlyconsonants . '<br>';
@@ -11,3 +11,9 @@ echo $onlyconsonants . '<br>';
 $b = "Breakfast at Tiffany's";
 $a = '2001: A Space Odyssey';
 $i = "It's a Wonderful Life";
+
+$onlyConsB = str_replace($vowels, "", $b);
+$onlyConsA = str_replace($vowels, "", $a);
+$onlyConsI = str_replace($vowels, "", $i);
+
+echo $onlyConsB . '<br>'. $onlyConsA . '<br>'. $onlyConsI;
---
+++ 010/strHw9.php
@@ -0,0 +1,4 @@
+<?php
+
+// Suskaičiuoti kiek stringe “Don't Be a Menace to South Central While Drinking Your Juice in the Hood” yra žodžių trumpesnių arba lygių nei 5 raidės. Pakartokite kodą su stringu “Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale”.
+

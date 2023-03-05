af3e7f2b06ad267844d0ac5bd2f45ca2926d8ac9
MonikaJaraite <monjara@gmail.com>
2023-03-05 19:54:13 +0200

    homework str ex. 10

---
+++ 010/strHw10.php
@@ -0,0 +1,19 @@
+<?php
+
+// Parašyti kodą, kuris generuotų atsitiktinį stringą iš lotyniškų mažųjų raidžių. Stringo ilgis 3 simboliai.
+
+$n = 3;
+function getRandomString($n)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+
+    return $randomString;
+}
+
+echo getRandomString($n);
--- 010/strHw9.php
+++ 010/strHw9.php
@@ -2,3 +2,6 @@
 
 // Suskaičiuoti kiek stringe “Don't Be a Menace to South Central While Drinking Your Juice in the Hood” yra žodžių trumpesnių arba lygių nei 5 raidės. Pakartokite kodą su stringu “Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale”.
 
+$d = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood";
+$t = 'Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale';
+

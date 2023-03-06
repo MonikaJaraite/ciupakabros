b40995f030528ccab588d6b483b9e0a6ad0dc32f
MonikaJaraite <monjara@gmail.com>
2023-03-06 19:24:56 +0200

    homework ex. C7

---
+++ 010/hwCiklai/hwC8.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rombas</title>
+    <!-- Reikia nupaišyti pilnavidurį rombą, taip pat, kaip ir pilnavidurį kvadratą (https://lt.wikipedia.org/wiki/Rombas), kurio aukštis 21 eilutė. Reikia padaryti, kad kiekviena rombo žvaigždutė būtų atsitiktinės RGB spalvos (perkrovus puslapį spalvos turi keistis). -->
+</head>
+<body>
+    
+</body>
+</html>
+

e74b263d52537c5e2c0f8548dba1f67118e8a51c
MonikaJaraite <monjara@gmail.com>
2023-03-07 11:43:10 +0200

    012 - masyvu failu iterpimas

---
+++ 010/hwCiklai/hwC11.php
@@ -0,0 +1,10 @@
+<?php
+
+// Sugeneruokite stringą, kurį sudarytų 50 atsitiktinių skaičių nuo 1 iki 200, atskirtų tarpais. Skaičiai turi būti unikalūs (t.y. nesikartoti). Sugeneruokite antrą stringą, pasinaudodami pirmu, palikdami jame tik pirminius skaičius (t.y tokius, kurie dalinasi be liekanos tik iš 1 ir patys savęs). Skaičius stringe sudėliokite didėjimo tvarka, nuo mažiausio iki didžiausio.
+
+$str = rand(1, 200);
+
+for ($x = 1; $x <= 50; ++$x) {
+    $rand = rand(1, 200);
+    
+}

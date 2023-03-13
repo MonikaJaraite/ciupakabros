312d3a3e8129c0c9f6819ad5aa61252c06e5bcd4
MonikaJaraite <monjara@gmail.com>
2023-03-13 12:36:36 +0200

    015

--- 015/forest/index.php
+++ 015/forest/index.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forest</title>
+    <title>Forest forMembers</title>
 </head>
 <body>
-    <?php require __DIR__ . '/menu.php' ?>
-    <h1>Welcome to the FOREST</h1>
+    <?php require __DIR__ . '/../menu.php' ?>
+    <h1 style="color: crimson;">Welcome to the DARK and DEEP FOREST</h1>
 </body>
 </html>
---
+++ 015/menu.php
@@ -0,0 +1,7 @@
+<?php
+defined('ENTER') || die('no entry here');
+?>
+<h2 style="color:darkgreen;">MENU</h2>
+<a href="http://localhost/ciupakabros/015/">HOME</a>
+<a href="http://localhost/ciupakabros/015/forest">DARK FOREST</a>
+<a href="http://localhost/ciupakabros/015/login">LOGIN</a>

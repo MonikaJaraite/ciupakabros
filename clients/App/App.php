031c2dbec5798af9052c580bf6601f31db15b524
MonikaJaraite <monjara@gmail.com>
2023-03-25 20:10:03 +0200

    Clients - Jo

--- clients/App/App.php
+++ clients/App/App.php
@@ -6,10 +6,22 @@ class App {
 
     public static function process() 
     {
-        
+
         $url = explode('/', $_SERVER['REQUEST_URI']);
         array_shift($url);
 
-        print_r($url);
+        return self::router($url);
+    }
+
+    private static function router(array $url) 
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'GET' && count($url) == 1 && $url[0] === '') {
+            return 'Jo';
+        }
+        else {
+            return 'Ne Jo';
+        }
     }
 }

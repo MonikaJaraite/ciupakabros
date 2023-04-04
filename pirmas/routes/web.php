e2f335594fdd6a3efd55c439925332a7cb3885b6
MonikaJaraite <monjara@gmail.com>
2023-04-04 21:21:19 +0300

    pirmas - lara

--- pirmas/routes/web.php
+++ pirmas/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PirmasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,16 @@ Route::get('/', function () {
 });
 
 Route::get('/labas', fn() => '<h1 style="color:crimson;">LABAS<h1>');
-Route::get('/labas/briedi', fn() => '<h1 style="color:crimson;">LABAS, BRIEDI<h1>');
+
+// Route::get('/labas/briedi', fn() => '<h1 style="color:crimson;">LABAS, BRIEDI<h1>');
+
+Route::prefix('labas')->group(function () {
+
+    Route::get('/briedi', [PirmasController::class, 'hello'])->name('briedis');
+    Route::get('/vovere', [PirmasController::class, 'helloV']);
+    Route::get('/{animal}', [PirmasController::class, 'helloAnimal']);
+    Route::get('/{animal}/{color}/color', [PirmasController::class, 'helloFancy'])->name('fancy');
+
+});
+
+Route::get('/sum/{a}/{b?}', [PirmasController::class, 'sum']);

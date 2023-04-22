5de798784f2534da93517a48a54b3e71eb82f883
MonikaJaraite <monjara@gmail.com>
2023-04-22 19:55:20 +0300

    Coro Store 035/1 - delete

--- color-store/app/Http/Controllers/CatController.php
+++ color-store/app/Http/Controllers/CatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cat;
+use Illuminate\Http\Request;
 
 class CatController extends Controller
 {
@@ -21,28 +22,34 @@ class CatController extends Controller
         ]);
     }
 
-    public function store(StoreCatRequest $request)
+    public function store(Request $request)
     {
-        //
-    }
-
-    public function show(Cat $cat)
-    {
-        //
+        Cat::create([
+            'title' => $request->title,
+            'colors_count' => $request->colors_count,
+        ]);
+        return redirect()->route('cats-index');
     }
 
     public function edit(Cat $cat)
     {
-        //
+        return view('back.cats.edit', [
+            'cat' => $cat
+        ]);
     }
 
-    public function update(UpdateCatRequest $request, Cat $cat)
+    public function update(Request $request, Cat $cat)
     {
-        //
+        $cat->update([
+            'title' => $request->title,
+            'colors_count' => $request->colors_count,
+        ]);
+        return redirect()->route('cats-index');
     }
 
     public function destroy(Cat $cat)
     {
-        //
+        $cat->delete();
+        return redirect()->route('cats-index');
     }
 }
